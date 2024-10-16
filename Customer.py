f8b9de0a2276a0fa5c20b0b0e2d4be3179b4e06c
lynn-sketch <110109942+lynn-sketch@users.noreply.github.com>
2024-10-16 20:21:07 +0300

    Add files via upload
    OOP ASSIGNMENT

---
+++ Customer.py
@@ -0,0 +1,15 @@
+
+class Customer:
+    def __init__(self, name: str, address: str):
+        self.name = name
+        self.address = address
+    # using dunder method __str__ to return customer information
+    def __str__(self):
+        return f'Customer Information: \nName: {self.name}\nAddress: {self.address}'
+
+
+if __name__ == '__main__':
+    # Creating customer class object
+    customer1 = Customer('AMOIT LYNN', 'MUKONO')
+    # Printing customer information
+    print(customer1)
